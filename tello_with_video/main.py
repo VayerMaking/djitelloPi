2932293c062c2551ba2b6a1dafe9c12bb35b144a
VayerMaking <martin.vayer@gmail.com>
2020-12-15 15:11:43 +0200

    not overwriting video but creating a new one

--- tello_with_video/main.py
+++ tello_with_video/main.py
@@ -1,6 +1,25 @@
 import time, cv2
 from threading import Thread
 from djitellopy import Tello
+import tempfile
+import itertools as IT
+import os
+
+def uniquify(path, sep = ''):
+    def name_sequence():
+        count = IT.count()
+        yield ''
+        while True:
+            yield '{s}{n:d}'.format(s = sep, n = next(count))
+    orig = tempfile._name_sequence
+    with tempfile._once_lock:
+        tempfile._name_sequence = name_sequence()
+        path = os.path.normpath(path)
+        dirname, basename = os.path.split(path)
+        filename, ext = os.path.splitext(basename)
+        fd, filename = tempfile.mkstemp(dir = dirname, prefix = filename, suffix = ext)
+        tempfile._name_sequence = orig
+    return filename
 
 tello = Tello()
 
@@ -13,7 +32,7 @@ frame_read = tello.get_frame_read()
 def videoRecorder():
     # create a VideoWrite object, recoring to ./video.avi
     height, width, _ = frame_read.frame.shape
-    video = cv2.VideoWriter('video.avi', cv2.VideoWriter_fourcc(*'XVID'), 30, (width, height))
+    video = cv2.VideoWriter(uniquify('video.avi'), cv2.VideoWriter_fourcc(*'XVID'), 30, (width, height))
 
     while keepRecording:
         video.write(frame_read.frame)
